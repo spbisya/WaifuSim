7c8e6ffc49cd6e52a1bd29101e7f51f65c1399fa
spbisya <wtcute.info@gmail.com>
2016-04-05 19:19:12 +0300

    Initial commit

---
+++ app/src/main/java/com/okunev/waifusim/MainActivity.java
@@ -0,0 +1,39 @@
+package com.okunev.waifusim;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Notification notification = new Notification.Builder(getApplicationContext())
+                .setContentTitle("Your Waifu")
+                .setContentText("Сходи пропукайся, на улице солнышко!")
+                .setSmallIcon(R.drawable.ic_stat_1444249298867)
+                .build();
+
+
+        // Sets an ID for the notification
+        int mNotificationId = 001;
+// Gets an instance of the NotificationManager service
+        NotificationManager mNotifyMgr =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+// Builds the notification and issues it.
+        mNotifyMgr.notify(mNotificationId, notification);
+
+    }
+
+}
