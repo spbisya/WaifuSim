5efdeb2cee0f8e1c3a794b33cd7a784e2f839d76
spbisya <wtcute.info@gmail.com>
2016-04-28 21:38:36 +0300

    Added sounds

--- app/src/main/java/com/okunev/waifusim/WaifuBatteryReceiver.java
+++ app/src/main/java/com/okunev/waifusim/WaifuBatteryReceiver.java
@@ -6,6 +6,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Uri;
 import android.os.BatteryManager;
 
 public class WaifuBatteryReceiver extends BroadcastReceiver {
@@ -21,25 +22,26 @@ public class WaifuBatteryReceiver extends BroadcastReceiver {
         int scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
         float batteryPct = (level / (float) scale) * 100;
         if (batteryPct < 15) {
-            setNot(context, "С-сенпай, не дай мне умереть!", 999);
+            setNot(context, "С-сенпай, не дай мне умереть!", 999, R.raw.low_battery);
             // do your task here.
         } else if (intent.getAction().equals(Intent.ACTION_POWER_CONNECTED)) {
-            setNot(context, "Нямка!!!", 999);
+            setNot(context, "Нямка!!!", 999, R.raw.power_connected);
         } else if (intent.getAction().equals(Intent.ACTION_POWER_DISCONNECTED) & batteryPct < 80) {
-            setNot(context, "Я не наелась!", 999);
+            setNot(context, "Я не наелась!", 999, R.raw.low_battery);
         } else if (intent.getAction().equals(Intent.ACTION_POWER_DISCONNECTED) & batteryPct > 80) {
-            setNot(context, "ВКУШНО!!!!", 999);
+            setNot(context, "ВКУШНО!!!!", 999, R.raw.power_disc_high);
         }
 
     }
 
-    public void setNot(Context context, String text, int id) {
+    public void setNot(Context context, String text, int id, int resId) {
         Notification notification = new Notification.Builder(context)
                 .setContentTitle("Your Waifu")
                 .setContentText(text)
                 .setSmallIcon(R.drawable.ic_stat_1444249298867)
+                .setSound(Uri.parse("android.resource://com.okunev.waifusim/" + resId))
                 .build();
-        notification.defaults |= Notification.DEFAULT_SOUND;
+        notification.defaults |= Notification.DEFAULT_LIGHTS | Notification.DEFAULT_VIBRATE;
 
         int mNotificationId = id;
         NotificationManager mNotifyMgr =
